9ee8a4cb569ac8868ef90f6ecf0dfd2e87107fa1
[Diogo Júnior] <diogofbjr@gmail.com]>
2023-06-06 11:57:33 -0300

    update

--- components/screens/LandingScreen/LandingScreen.tsx
+++ components/screens/LandingScreen/LandingScreen.tsx
@@ -8,9 +8,6 @@ import {
   HelpText,
   Button,
   Alert,
-  Callout,
-  CalloutHeading,
-  CalloutText,
 } from "@twilio-paste/core";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
@@ -18,7 +15,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 import { CenterContent, MaxWidthDiv } from "../../styled";
-import TwilioHeading from "../../TwilioHeading/TwilioHeading";
 import { useVideoStore, VideoAppState, UIStep } from "../../../store/store";
 import { TEXT_COPY } from "../../../lib/constants";
 import { getPermissionStatus } from "../../../lib/utils/devices";
